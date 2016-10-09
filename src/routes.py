d8ceacb4797b00c619ba9bc0c9e0c26eaa42fe08
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-10-09 20:15:26 +0200

    Thor can tell if dimension in incorrect

--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request
+import json
+
 thor = Flask(__name__)
+
 
 @thor.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@thor.route('/api/<dimension>', methods=["GET", "POST"])
+def api(dimension):
+    # None if args not given as json
+    arguments = request.get_json()
+    # Set arguments from URL
+    if arguments == None:
+        arguments = request.args.to_dict(flat=False)
+
+    if dimension == "temperature":
+        pass
+    elif dimension == "air-pressure":
+        pass
+    elif dimension == "precipitation":
+        pass
+    elif dimension == "water-level":
+        pass
+    else:
+        return json.dumps({"ok": False, "error": "Client sent incorrect dimension: " + dimension})
--- src/thor.py
+++ src/thor.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from routes import *
+import reader
 
 if __name__ == "__main__":
+    
+    files = []
+
     thor.run()
