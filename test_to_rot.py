a55da8fa0585b9c872f947c969a033e547172384
Alexander Poole <alex.o.poole@gmail.com>
2016-11-08 11:05:17 +0100

    Made it so it requires input just like test_to_reg.py

--- test_to_rot.py
+++ test_to_rot.py
@@ -5,11 +5,9 @@ from numpy import *
 from thor.transform import *
 import netCDF4
 
-filename = "ncFiles/tas_EUR-11_CNRM-CERFACS-CNRM-CM5_rcp45_r1i1p1_SMHI-RCA4_v1_mon_200601-201012.nc"
-ncFile = netCDF4.Dataset(filename, 'r')
 
-lon = -10.0638796622 #ncFile.variables['rlat'][0]
-lat = 21.9878287568 #ncFile.variables['rlon'][0]
+lon = float(sys.argv[1]) #ncFile.variables['rlat'][0]
+lat = float(sys.argv[2]) #ncFile.variables['rlon'][0]
 
 print("lon: " + str(lon) + " lat: " + str(lat))
 
