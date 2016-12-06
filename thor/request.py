6d25f0441925e8847e960cbfb9f741fed64721ec
Alexander Poole <alex.o.poole@gmail.com>
2016-12-06 15:45:16 +0100

    Fixed bug

--- thor/request.py
+++ thor/request.py
@@ -47,33 +47,9 @@ def overlapScale(overlapArea,
 
 def handleRequest(arguments, ncFileDictTree, log):
 
-    # ---------------
-    """ TODO: Until we expose different climate models within the API
-     - we just set default values for them """
     variable = arguments["variable"]
-    if "domain" not in arguments:
-        domain = list(ncFileDictTree.keys())[0]
-    elif arguments["domain"] not in list(ncFileDictTree.keys()):
-        return {"ok": False,
-                "error": "No files with specified domain."}
-    else:
-        domain = str(arguments["domain"])
-    if "climate-model" not in arguments:
-        model = list(ncFileDictTree[domain].keys())[0]
-    elif arguments["climate-model"] not in list(ncFileDictTree[
-            domain].keys()):
-        return {"ok": False,
-                "error": "No files with specified climate-model."}
-    else:
-        model = str(arguments["climate-model"])
-    if "exhaust-level" not in arguments:
-        experiment = list(ncFileDictTree[domain][model].keys())[0]
-    elif arguments["exhaust-level"] not in list(ncFileDictTree[
-            domain][model].keys()):
-        return {"ok": False,
-                "error": "No files with specified exhaust-level."}
-    else:
-        experiment = str(arguments["exhaust-level"])
+    model = str(arguments["climate-model"])
+    experiment = str(arguments["exhaust-level"])
 
     # ---------------
     requestedFiles = getReaderList(ncFileDictTree,
