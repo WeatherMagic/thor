1b078632106c1ee1ad2457943dbcd0f2cea9db75
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-12-08 18:26:19 +0100

    Comments for documentation purpose

--- thor/const.py
+++ thor/const.py
@@ -3,11 +3,21 @@ import sys
 
 
 class _const:
+    """
+    Purpose: Provide variables which cannot be
+    reset once they've been assigned a value.
+
+    example:
+        a = 10
+        a = 11 # This will throw an exception
+    """
     class ConstError(TypeError):
         pass
 
     def __setattr__(self, name, value):
+        # If const.name exists
         if name in self.__dict__:
+            # Throw exception
             raise self.ConstError("Can't rebind const(%s)" % name)
         self.__dict__[name] = value
 
