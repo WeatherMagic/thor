b941374705d7bcab7b1bc3495c11025189e025b5
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-12-11 16:49:50 +0100

    Special case for historical

--- scripts/populateCache.bash
+++ scripts/populateCache.bash
@@ -11,7 +11,7 @@ fi
 resolution=$1
 domains="EUR-44i CAM-44i SAM-44i NAM-44i MNA-44i EUR-11i MNA-22i ANT-44i WAS-44i AFR-44i ARC-44i"
 models="ICHEC-EC-EARTH IPSL-IPSL-CM5A-MR CNRM-CERFACS-CNRM-CM5"
-exhaustLevels="rcp45 historical rcp85"
+exhaustLevels="rcp45 rcp85"
 
 
 for domain in $domains; do
@@ -20,6 +20,23 @@ for domain in $domains; do
             for year in $(seq 2006 2100); do
                 for month in $(seq 1 12); do
                     curlString="http://thor.hfelo.se/api/temperature?to-latitude=90&month=$month&climate-model=$model&year=$year&height-resolution=$resolution&exhaust-level=$exhaustLevel&from-latitude=-90&from-longitude=-180&to-longitude=180" 
+                    echo "Fetching: "
+                    echo $curlString 
+                    curl $curlString > /dev/null
+                    echo ""
+                done
+            done
+        done
+    done
+done
+
+# Historical as well
+for domain in $domains; do
+    for model in $models; do
+        for exhaustLevel in $exhaustLevels; do
+            for year in $(seq 1950 2005); do
+                for month in $(seq 1 12); do
+                    curlString="http://thor.hfelo.se/api/temperature?to-latitude=90&month=$month&climate-model=$model&year=$year&height-resolution=$resolution&exhaust-level=historical&from-latitude=-90&from-longitude=-180&to-longitude=180" 
                     echo "Fetching: "
                     echo $curlString 
                     curl $curlString > /dev/null
