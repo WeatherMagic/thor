9e7c7b98282e55b66a0cdf3835e3c65fefee64bf
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-12-11 19:46:46 +0100

    Stopped looping over unnecessary variable

--- scripts/populateCache.bash
+++ scripts/populateCache.bash
@@ -29,15 +29,13 @@ done
 
 # Historical as well
 for model in $models; do
-    for exhaustLevel in $exhaustLevels; do
-        for year in $(seq 1950 2005); do
-            for month in $(seq 1 12); do
-                curlString="http://thor.hfelo.se/api/temperature?to-latitude=90&month=$month&climate-model=$model&year=$year&height-resolution=$resolution&exhaust-level=historical&from-latitude=-90&from-longitude=-180&to-longitude=180" 
-                echo "Fetching: "
-                echo $curlString 
-                curl $curlString > /dev/null
-                echo ""
-            done
+    for year in $(seq 1950 2005); do
+        for month in $(seq 1 12); do
+            curlString="http://thor.hfelo.se/api/temperature?to-latitude=90&month=$month&climate-model=$model&year=$year&height-resolution=$resolution&exhaust-level=historical&from-latitude=-90&from-longitude=-180&to-longitude=180" 
+            echo "Fetching: "
+            echo $curlString 
+            curl $curlString > /dev/null
+            echo ""
         done
     done
 done
